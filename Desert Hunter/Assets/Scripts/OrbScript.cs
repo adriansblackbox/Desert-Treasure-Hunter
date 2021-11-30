54dee59571713fbfaa8680aeeb442868ba0f208f
Adrian <adrianlance86@gmail.com>
2021-11-30 15:58:32 -0800

    made orb nice

--- Desert Hunter/Assets/Scripts/OrbScript.cs
+++ Desert Hunter/Assets/Scripts/OrbScript.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
+using UnityEditor.VFX;
+using UnityEditor.VFX.UI;
 
 public class OrbScript : MonoBehaviour
 {
@@ -9,17 +12,14 @@ public class OrbScript : MonoBehaviour
         _bladeScript = FindObjectOfType<Blade_Controller>();
     }
     private void Update() {
-        
     }
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("Blade")){
             _bladeScript.t = 0.0f;
-            GetComponent<MeshRenderer>().enabled = false;
-            GetComponent<Collider>().enabled = false;
+            this.gameObject.SetActive(false);
         }
     }
     public void Reset() {
-        GetComponent<MeshRenderer>().enabled = true;
-        GetComponent<Collider>().enabled = true;
+        this.gameObject.SetActive(true);
     }
 }
